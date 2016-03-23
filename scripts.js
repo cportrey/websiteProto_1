e3b6bf704e2d5faff3e4b8f7938166219e451097
cportrey <portreyc@gmail.com>
2016-03-23 18:06:18 -0400

    Adding Angular array and YT player

---
+++ scripts.js
@@ -0,0 +1,65 @@
+var app = angular.module('app', []);
+
+app.controller('mainController', function ($scope) {
+
+    $scope.videoList = [
+     '74gTaRy5GWY',
+     'M7lc1UVf-VE'
+   ];
+
+    $scope.add = function () {
+        $scope.videoList.push($scope.input);
+        $scope.input = '';
+    };
+
+    ytList = $scope.videoList;
+
+
+
+});
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.loadPlaylist(ytList);
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
